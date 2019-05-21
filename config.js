715bdeee5b61eea912149f8f9d429736908a2c10
David.T.Nguyen <david.t.nguyen@target.com>
2019-05-20 22:18:25 -0500

    update author. title

--- config.js
+++ config.js
@@ -1,38 +1,40 @@
-'use strict';
+'use strict'
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
-  postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
-  menu: [
-    {
-      label: 'Articles',
-      path: '/'
-    },
-    {
-      label: 'About me',
-      path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
-    }
-  ],
-  author: {
-    name: 'John Doe',
-    photo: '/headshot.png',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-    contacts: {
-      email: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '#',
-      vkontakte: '#'
-    }
-  }
-};
+	url: 'https://lumen.netlify.com',
+	title: 'Blog by David Nguyen',
+	subtitle:
+		'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+	copyright: '© All rights reserved.',
+	disqusShortname: '',
+	postsPerPage: 4,
+	googleAnalyticsId: 'UA-73379983-2',
+	menu: [
+		{
+			label: 'Articles',
+			path: '/',
+		},
+		{
+			label: 'About me',
+			path: '/pages/about',
+		},
+		{
+			label: 'Contact me',
+			path: '/pages/contacts',
+		},
+	],
+	author: {
+		name: 'David Nguyen',
+		photo: '/headshot.png',
+		bio:
+			'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+		contacts: {
+			email: '#',
+			telegram: '#',
+			twitter: '#',
+			github: '#',
+			rss: '#',
+			vkontakte: '#',
+		},
+	},
+}
